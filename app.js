1a17d34db98d3722d5f4fe2180ee82d3000120b9
Hank H'ng <hankhng@hotmail.com>
2022-01-17 09:25:35 +0000

    WIP node.js

--- app.js
+++ app.js
@@ -1,5 +1,52 @@
-function sayHello (name) {
-  console.log("Hello " + name);
-}
+const log = require('./logger');
 
-sayHello('world');
+log('message');
+
+// function sayHello (name) {
+//   console.log("Hello " + name);
+// }
+
+// sayHello('world');
+
+// --------------------------------------------------
+
+// console.log(sayHello); -> is a console.log is a global function
+
+// setTimeout()
+// clearTimeout()
+// setInterval()
+// clearInterval()
+
+
+// No window object in node
+
+// defining a function adds it to the global scope, if you declare it in multiple places it will overwrite the previous one
+// this is a problem, we should avoid defining functions in the global scope
+
+// var sayHello = function () {
+
+// }
+
+// instead we need modularity
+
+// every node project should have a main module, i.e. app.js
+
+// -----------------------------------------------------
+
+// console.log(module);
+//  then run app.js
+
+// -----------------------------------------------------
+
+// -----------------------------------------------------
+
+// -----------------------------------------------------
+
+
+
+
+
+
+
+
+
